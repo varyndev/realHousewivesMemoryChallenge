3372ee000c5613af6c284580fa73b9cadd2fee51
John Foster <john@varyn.com>
2014-09-04 17:28:42 -0400

    Release Candidate for version 80

--- package.sh
+++ package.sh
@@ -1,2 +1,6 @@
-zip -r -X ./distrib/RH-MemoryMatch.zip ./distrib/ -x "*.zip" -x "./distrib/.DS_Store"
+# Package the contents of the distrib folder for delivery.
+# Assumes you already have the distrib folder up to date.
+#
+rm ./distrib/RH-MemoryMatch.zip
+zip -r -X ./distrib/RH-MemoryMatch.zip ./distrib/ -x "*.zip" -x "*.DS_Store"
 
